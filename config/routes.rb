dedcc8a2c1a8cc7553f005dcea3136c461c9231b
github username <github email address>
2019-04-04 18:31:10 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+resources :songs, :authors, only: [:new, :show, :index, :create, :edit, :update, :delete]
 end
