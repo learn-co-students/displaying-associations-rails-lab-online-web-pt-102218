3dd92ee101bf6c4448b14def66a608ab04ecc8cf
Paul Ashour <paul.ashour@gmail.com>
2019-04-16 20:21:50 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+sixty = Song.create( title: "When I'm Sixty-Four")
+circus = Song.create( title: "Circus")
+viva = Song.create( title: "Viva la Vida")
+dont = Song.create( title: "Don't Let's Start")
+three = Song.create( title: "Number Three")
+lost = Song.create( title: "Lost My Mind")
+
+Artist.create( name: "The Beatles", songs:[sixty])
+Artist.create( name: "Britney Spears", songs:[circus])
+Artist.create( name: "Coldplay", songs:[viva])
+Artist.create( name: "They Might Be Giants", songs:[dont, three, lost])
